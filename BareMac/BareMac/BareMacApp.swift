5e7aae442ee466c6a58db054c1ed5df1280ad697
mercha <136171521+m3rcha@users.noreply.github.com>
2025-04-26 17:00:26 +0300

    feat: bootstrap SwiftUI MVP

---
+++ BareMac/BareMac/BareMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  BareMacApp.swift
+//  BareMac
+//
+//  Created by Ege Ozten on 26.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct BareMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BareMac/BareMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BareMac
+//
+//  Created by Ege Ozten on 26.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
